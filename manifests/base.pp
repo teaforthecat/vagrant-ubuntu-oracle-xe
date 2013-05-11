ce43baa3048d8080fa86254867e9b38963a0a7c7
Chris Thompson <cthompson@Chriss-MacBook-Pro.local>
2013-05-11 17:25:28 -0500

    add db-setup module with tyler's sql

--- manifests/base.pp
+++ manifests/base.pp
@@ -2,6 +2,7 @@ node oracle {
   include oracle::server
   include oracle::swap
   include oracle::xe
+  include db-setup
 
   user { "vagrant":
     groups => "dba",
---
+++ modules/db-setup/manifests/init.pp
@@ -0,0 +1,15 @@
+# -L for don't prompt on error
+# sqlplus -L system/manager@//localhost:1521/XE @/Users/cthompson/vagrant/vagrant-ubuntu-oracle-xe/modules/db-setup/files/create_APP_DBO
+
+class db-setup {
+
+  file { '/tmp/create-users-and-tbsps.sql':
+    source => "puppet:///modules/db-setup/local_oracle_tyler.sql";
+  }
+  ->
+  exec {'create-users-and-tbsps':
+    command => "sqlplus -L system/manager@localhost:1521/XE @/tmp/create-users-and-tbsps",
+    path    => '/u01/app/oracle/product/11.2.0/xe/bin/',
+  }
+
+}
